410fbf78530134c5cb6da59f92e4ebb245bd4357
R_Hasan <sohandoabernahin1@gmail.com>
2020-12-23 18:11:22 +0600

    ki korlam k jane :/

--- Portfolio 001/script.js
+++ Portfolio 001/script.js
@@ -10,7 +10,6 @@ if(theme == null){
 
 let themeDots = document.getElementsByClassName('theme-dot')
 
-
 for (var i=0; themeDots.length > i; i++){
 	themeDots[i].addEventListener('click', function(){
 		let mode = this.dataset.mode
